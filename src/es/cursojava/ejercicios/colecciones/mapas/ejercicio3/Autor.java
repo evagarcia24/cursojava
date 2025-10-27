78a88b938e2ec469bc715fbc77540f74be2c59e9
Eva <eva@email.com>
2025-10-27 16:33:39 +0100

    ejercicio3

--- src/es/cursojava/ejercicios/colecciones/mapas/ejercicio3/Autor.java
+++ src/es/cursojava/ejercicios/colecciones/mapas/ejercicio3/Autor.java
@@ -1,9 +1,61 @@
 package es.cursojava.ejercicios.colecciones.mapas.ejercicio3;
 
+import java.util.Objects;
+
 public class Autor {
 
-		private String nombre;
-		private String nacionalidad;
+	private String nombre;
+	private String nacionalidad;
 
+	// Constructor
+	public Autor(String nombre, String nacionalidad) {
+		super();
+		this.nombre = nombre;
+		this.nacionalidad = nacionalidad;
+	}
+
+	// Getters y Setters
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getNacionalidad() {
+		return nacionalidad;
+	}
+
+	public void setNacionalidad(String nacionalidad) {
+		this.nacionalidad = nacionalidad;
+	}
+
+	// Metodo toString sobreescrito
+	@Override
+	public String toString() {
+		return "Autor [nombre=" + nombre + ", nacionalidad=" + nacionalidad + "]";
+	}
+
+	// Método hashCode sobreescrito
+	@Override
+	public int hashCode() {
+		return Objects.hash(nacionalidad, nombre);
+	}
+
+	// Método equals sobreescrito
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Autor other = (Autor) obj;
+		return Objects.equals(nacionalidad, other.nacionalidad) && Objects.equals(nombre, other.nombre);
+	}
 	
+	
+
 }
--- src/oo/MapaColegio2.java
+++ src/oo/MapaColegio2.java
@@ -2,6 +2,7 @@ package oo;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,66 +15,78 @@ public class MapaColegio2 {
 	public static void main(String[] args) {
 		Colegio colegio1 = new Colegio("Cole1", "Dir1");
 		Colegio colegio2 = new Colegio("Cole2", "Dir2");
-		
-		
+
 		List<Alumno> alumnos = MapaColegio.generaAlumnos();
-		
-		Map<String,List<Alumno>> aulasCole3 = new HashMap<>();
-		aulasCole3.put("aula1",alumnos.subList(0, 2));
-		aulasCole3.put("aula2",alumnos.subList(2, 3));
-		aulasCole3.put("aula3",alumnos.subList(3, 7));
-		Colegio colegio3 = new Colegio("Cole3", "Dir3",aulasCole3);
-		
-		Map<String,List<Alumno>> aulasCole2 = new HashMap<>();
+
+		Map<String, List<Alumno>> aulasCole3 = new HashMap<>();
+		aulasCole3.put("aula1", alumnos.subList(0, 2));
+		aulasCole3.put("aula2", alumnos.subList(2, 3));
+		aulasCole3.put("aula3", alumnos.subList(3, 7));
+		Colegio colegio3 = new Colegio("Cole3", "Dir3", aulasCole3);
+
+		Map<String, List<Alumno>> aulasCole2 = new HashMap<>();
 		aulasCole2.put("aula1", alumnos.subList(7, 10));
 		colegio2.setAulas(aulasCole2);
-		
-		
-		List<Colegio> colegios = Arrays.asList(colegio1,colegio2, colegio3);
+
+		List<Colegio> colegios = Arrays.asList(colegio1, colegio2, colegio3);
 		for (Colegio colegio : colegios) {
 			if (colegio.getAulas().isEmpty()) {
-				System.out.println(colegio.getNombre() +" sin aulas");
-				Map<String,List<Alumno>> aulasCole1 = new HashMap<>();
+				System.out.println(colegio.getNombre() + " sin aulas");
+				Map<String, List<Alumno>> aulasCole1 = new HashMap<>();
 				aulasCole1.put("aula1", alumnos.subList(10, 11));
 				aulasCole1.put("aula2", alumnos.subList(11, 12));
 				aulasCole1.put("aula3", alumnos.subList(12, 13));
-				
+
 				colegio.setAulas(aulasCole1);
 			}
 		}
 		Alumno alumnoN = new Alumno("AN", "222N", 9.5);
 		Alumno alumnoN1 = new Alumno("AN1", "222N1", 6.5);
-		//colegio2.getAulas().get("aula1").add(alumnoN);//Añadimos alumnos a un aula que ya existe
-		
+		// colegio2.getAulas().get("aula1").add(alumnoN);//Añadimos alumnos a un aula
+		// que ya existe
+
 		colegio2.getAulas().put("aula2", new ArrayList(Arrays.asList(alumnoN)));
 		colegio2.getAulas().get("aula2").add(alumnoN1);
-		//System.out.println(colegio2.getAulas().get("aula2").size());
+		// System.out.println(colegio2.getAulas().get("aula2").size());
 
-		
 		mostrarDatosColegios(colegios);
 	}
-	
-	private static void mostrarDatosColegios (List<Colegio> colegios) {
-		
+
+	private static void mostrarDatosColegios(List<Colegio> colegios) {
+
 		for (Colegio colegio : colegios) {
 			System.out.println(colegio);
-			Map<String,List<Alumno>> aulas= colegio.getAulas();
-			Set<String> nombresAulas =  aulas.keySet();
+			Map<String, List<Alumno>> aulas = colegio.getAulas();
+			Set<String> nombresAulas = aulas.keySet();
 			for (String nombreAula : nombresAulas) {
-				System.out.println("\t"+nombreAula);
+				System.out.println("\t" + nombreAula);
 				List<Alumno> alumnos = aulas.get(nombreAula);
 				for (Alumno alumno : alumnos) {
-					System.out.println("\t\t"+alumno);
+					System.out.println("\t\t" + alumno);
 				}
 			}
 		}
+
+	}
+
+	// Indicar cual es el colegio que tiene la nota media de alumnos mayor.
+	private static void mostrarColegioNotaMediaMayor (List<Colegio> colegios) {
+			   
+		for (Colegio colegio : colegios) {
+			System.out.println(colegio.getNombre());
+			Map<String, List<Alumno>> aulas = colegio.getAulas();
+			Collection<List<Alumno>> listadosAlumnos = aulas.values();
+			
+			for (List<Alumno> alumnos : listadosAlumnos) {
+				for (Alumno alumno : alumnos) {
+					System.out.println(alumno);
+				}
+			}
 		
-		
+		}
 		
 	}
 
+
+
 }
-
-
-
-
