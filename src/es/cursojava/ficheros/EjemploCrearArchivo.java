d86262a0a1ef86cba7249c63211d2e81936d0a33
Eva <email.email.com>
2025-11-28 13:46:08 +0100

    proyecto clonado

--- src/es/cursojava/ficheros/EjemploCrearArchivo.java
+++ src/es/cursojava/ficheros/EjemploCrearArchivo.java
@@ -7,33 +7,39 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 public class EjemploCrearArchivo {
-	public static void main(String[] args) {
-		LocalDateTime datetime = LocalDateTime.now();
-		System.out.println(datetime);
-		System.out.println(datetime.getYear());
-		System.out.println(datetime.getMonthValue());
-		System.out.println(datetime.getMonth());
-
-		String fechaStr = "" + datetime.getYear() + datetime.getMonthValue() + datetime.getDayOfMonth();
-
-		LocalDate date = LocalDate.now(); // Saca solo año, mes y dia
-		String fechaStr2 = "" + date;
-
-		Instant instant = Instant.now();
-		System.out.println(instant);
-		// Ruta relativa a la carpeta del proyecto
-
+    public static void main(String[] args) {
+//    	LocalDateTime datetime = LocalDateTime.now();
+//		System.out.println(datetime);
+//    	System.out.println(datetime.getYear());
+//    	System.out.println(datetime.getMonthValue());
+//    	System.out.println(datetime.getMonth());
+//    	
+//		String fechaStr = ""+ datetime.getYear() + datetime.getMonthValue() + datetime.getDayOfMonth();
+//
+//		LocalDate date = LocalDate.now();
+//		String fechaStr2 = ""+date;
+//		System.out.println("============================");
+//		System.out.println(fechaStr2);
+//		
+//		System.out.println("============================");
+//    	Instant instant = Instant.now();
+//    	System.out.println(instant);
+//		//Ruta relativa a la carpeta del proyecto
+//
 		Date fecha = new Date();
-		SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy"); // m en minuscula es mes y M es mes
+		SimpleDateFormat df = new SimpleDateFormat("ddMMyyyy");
 		String fechaStr3 = df.format(fecha);
-
+		
 		System.out.println(fechaStr3);
-
-		String nombreArchivo = "./recursos/eva_" + fechaStr3 + ".txt";
+		
+		
+		
+		System.out.println("============================");
+        String nombreArchivo = "./recursos/joseluis_"+ fechaStr3 +".txt";
 		System.out.println(nombreArchivo);
-
-		ArchivoServicio service = new ArchivoServicio();
-		// service.mostrarContenidoDirectorio("C:\\Users\\Tardes\\");
-		service.crearArchivo(nombreArchivo);
-	}
+		
+        ArchivoServicio service = new ArchivoServicio();
+        //service.mostrarContenidoDirectorio("C:\\Users\\Tardes\\");
+        service.crearArchivo3(nombreArchivo);
+    }
 }
--- src/es/cursojava/ficheros/EmpezandoConFicheros.java
+++ src/es/cursojava/ficheros/EmpezandoConFicheros.java
@@ -12,6 +12,10 @@ public class EmpezandoConFicheros {
         File directorio = new File("C:\\Users\\Tardes\\Ficheros");
         File fichero = new File("C:\\Users\\Tardes\\Ficheros\\fichero1.txt");
         
+        if (!directorio.exists()) {
+        	directorio.mkdir();
+        }
+        
         System.out.println(directorio.isDirectory());
         System.out.println(directorio.isFile());
 
@@ -19,29 +23,39 @@ public class EmpezandoConFicheros {
         System.out.println(fichero.isFile());
         System.out.println(fichero.exists());
 
-        for (int i = 0; i < 10; i++) {
-            File directorioNuevo = new File (directorio,"\\subdir"+i);
-            directorioNuevo.mkdir();
-        }
+//        for (int i = 0; i < 10; i++) {
+//            File directorioNuevo = new File(directorio,"\\subdir"+i);
+//            directorioNuevo.mkdir();
+//        }
+//        System.out.println("=========================================");
+//        String[] lista = directorio.list();
+//        for (String nombre : lista) {
+//            System.out.println(nombre);
+//            File file1 = new File(directorio,nombre);
+//            file1.delete();
+//        }
+//        
+//        System.out.println("=========================================");
+//        lista = directorio.list();
+//        for (String nombre : lista) {
+//            System.out.println(nombre);
+//        }
+//        
+//        System.out.println("=========================================");
+//
+//        File [] ficheros = directorio.listFiles();
+//        for (File file : ficheros) {
+//            System.out.println(file.delete());
+//        }
+//
         System.out.println("=========================================");
-        String[] lista = directorio.list();
-        for (String nombre : lista) {
-            System.out.println(nombre);
-            File file1 = new File(directorio,nombre);
-            file1.delete();
-        }
-
-        File [] ficheros = directorio.listFiles();
-        for (File file : ficheros) {
-            System.out.println(file.delete());
-        }
-
-        System.out.println("=========================================");
-        File fichero2 = new File("C:\\Users\\Tardes\\Ficherosss\\fichero2.txt");
+        File fichero2 = new File("C:\\Users\\Tardes\\Ficheros\\fichero2.txt");
         try {
             fichero2.createNewFile();
         } catch (IOException e) {
-            logger.error("Error al crear el fichero "+ fichero2.getAbsolutePath() + ": " + e.getMessage(), e);
+            logger.error("Error al crear el fichero "+ 
+            		fichero2.getAbsolutePath() + ": " + e.getMessage() +
+            		fichero2.getPath() + " "); 
         }
 
 
